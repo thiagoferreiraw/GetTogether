2283b4ad21472acea2cc7b4b61cc979db6a5baec
Thiago <thiago.ferreira@cravefood.services>
2019-04-10 22:45:28 -0300

    Added media root

--- get_together/settings.py
+++ get_together/settings.py
@@ -184,8 +184,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
-MEDIA_ROOT = './media/'
+# MEDIA_ROOT = './media/'
 MEDIA_URL = '/media/'
 
 MATOMO_HOST=None
